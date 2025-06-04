15ecc3cb7a8396c2e46faade656b5122448b7b15
AdeshBarve <adesh.barve20@gmail.com>
2025-06-04 21:32:58 +0530

    Removel of server

--- api/index.js
+++ api/index.js
@@ -14,5 +14,5 @@ connectDB(); // Connect to MongoDB
 
 app.use('/api/auth', require('./routes/auth'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
